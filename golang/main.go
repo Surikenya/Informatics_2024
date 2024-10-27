113b91fdbdbb811965b4f3b1fee4fd2ad05100f4
Dasha_Hljupina <dashahljupina@gmail.com>
2024-10-27 18:10:46 +0300

    hljupina laba6

--- golang/main.go
+++ golang/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	"isuct.ru/informatics2022/laba4ab"
+	"isuct.ru/informatics2022/laba6"
 )
 
 func main() {
 	fmt.Println("Hljupina Daria")
-	laba4ab.Readylaba4ab()
+	laba6.Readylaba6()
 }
