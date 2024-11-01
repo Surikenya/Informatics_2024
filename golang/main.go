d1bb7d5362ed098212165abdd58d6f33a3dc9d84
Dasha_Hljupina <dashahljupina@gmail.com>
2024-11-01 15:20:50 +0300

    as

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/la4"
+	"isuct.ru/informatics2022/la6"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Hljupina Daria")
+	la4.Readyla4()
+	la6.Readyla6()
 }
