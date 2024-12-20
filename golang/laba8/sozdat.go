835227d15128584ca75a5f1e35ca95ff80e393f0
Dasha_Hljupina <dashahljupina@gmail.com>
2024-12-20 21:06:35 +0300

    laba8

---
+++ golang/laba8/sozdat.go
@@ -0,0 +1,21 @@
+package laba8
+
+import (
+	"fmt"
+	"os"
+)
+
+func SozdanieFile(fileName string) error {
+	_, err := os.Stat(fileName)
+	if err == nil {
+		return fmt.Errorf("файл с таким названием уже существует")
+	}
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("не удалось создать файл ошибка: %w", err)
+	}
+	defer file.Close()
+
+	return nil
+}
